62d87a90e390b6093ddd8da732b87835f8baaaea
enissonshilo <114188657+enissonshilo@users.noreply.github.com>
2023-07-01 00:15:40 -0300

    updated post-list images

--- src/data.ts
+++ src/data.ts
@@ -8,20 +8,38 @@ export interface IPost {
 export const Posts: IPost[] = [
   {
     id: 1,
-    title: "Somos tão jovens",
-    imageUrl: "../images/somos-tao-jovens.jpg",
-    body: "Somos tão Jovens conta a emocionante e desafiadora história da transformação de Renato Manfredini Jr. no mito Renato Russo, revelando como um rapaz de Brasília, no final da ditadura, criou canções como 'Que País é Este', 'Música Urbana', 'Geração Coca-Cola', 'Eduardo e Mônica' e 'Faroeste Caboclo', verdadeiros hinos da juventude urbana dos anos 80 que continuam a ser cultuadas geração após geração por uma crescente legião de jovens fãs.",
+    title: "Christ the Redeemer",
+    imageUrl: "../images/rio-de-janeiro.jpg",
+    body: "Perched atop Corcovado Mountain in Rio de Janeiro, Brazil, Christ the Redeemer is an iconic statue that overlooks the city. Symbolizing peace and welcoming visitors with open arms, this colossal statue is one of the New Seven Wonders of the World.",
   },
   {
     id: 2,
-    title: "titulo",
-    imageUrl: "../images/a-negociacao.jpg",
-    body: "Um magnata está fora de si e ansioso para vender seu império antes que suas fraudes sejam reveladas, mas as coisas não saem exatamente como ele planejou e ele terá que buscar ajuda em lugares inusitados.",
+    title: "Macchu Pichu",
+    imageUrl: "../images/machu-picchu.jpg",
+    body: "Nestled high in the Andes Mountains of Peru, Machu Picchu is an ancient Incan citadel and a UNESCO World Heritage Site. This mystical wonder is renowned for its incredible stone architecture and breathtaking mountainous backdrop. Exploring the terraces, temples, and intricate stone pathways of Machu Picchu offers a glimpse into the ingenuity and spirituality of the Incan civilization.",
   },
   {
-    id: 2,
-    title: "titulo",
-    imageUrl: "../images/o-poder-e-o-impossível.jpg",
-    body: "Eric (Josh Hartnett) é um jovem rebelde à procura de respostas. Durante um fim de semana ele resolve se hospedar em uma estação de esqui. Tudo que quer é a tranquilidade da natureza e praticar seu esporte favorito. Depois de algum tempo explorando a área ele é pego inesperadamente por uma gigantesca tempestade de neve. Perdido, e sem conseguir contato com ninguém, somente algo extraordinário poderá salva-lo. Baseado em uma história real e no livro de grande sucesso.",
+    id: 3,
+    title: "Taj Mahal",
+    imageUrl: "../images/taj-mahal.jpg",
+    body: "The Taj Mahal, located in Agra, India, is an exquisite white marble mausoleum and a symbol of love. Built by Emperor Shah Jahan in memory of his wife, it is considered a masterpiece of Mughal architecture. The intricate carvings, shimmering marble, and perfectly symmetrical design of the Taj Mahal make it a timeless wonder that attracts millions of visitors every year.",
+  },
+  {
+    id: 4,
+    title: "Colosseum",
+    imageUrl: "../images/coliseu.jpg",
+    body: "The Colosseum, situated in Rome, Italy, is an iconic amphitheater and a remarkable architectural feat. Built during the Roman Empire, it was a venue for gladiator fights and grand spectacles. Today, the Colosseum stands as a symbol of ancient Roman civilization and attracts visitors who marvel at its grandeur and historical significance.",
+  },
+  {
+    id: 5,
+    title: "Chichén Itzá",
+    imageUrl: "../images/chichen-itza.jpg",
+    body: "Chichén Itzá, located in the Yucatán Peninsula of Mexico, is a fascinating archaeological site and one of the Seven Wonders of the World. It was once a major Mayan city and showcases impressive structures, such as the iconic El Castillo pyramid. Exploring the intricate carvings, ancient ball courts, and astronomical observatory at Chichén Itzá is a journey into the rich history and culture of the Mayan civilization.",
+  },
+  {
+    id: 6,
+    title: "The Great Wall of China",
+    imageUrl: "../images/muralha-da-china.jpg",
+    body: "The Great Wall of China is a colossal architectural wonder that stretches over 13,000 miles. Built to protect China from invasions, it is the longest man-made structure in the world. Its historical significance and breathtaking views make it an unforgettable wonder.",
   },
 ];
