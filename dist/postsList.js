43f253e6eb788d32311865b2511355cd5511d1da
enissonshilo <114188657+enissonshilo@users.noreply.github.com>
2023-07-09 15:13:26 -0300

    javascript files added

---
+++ dist/postsList.js
@@ -0,0 +1,20 @@
+import { Posts } from "./data.js";
+function createPost() {
+    Posts.forEach((post) => {
+        const postBody = `
+  <article class="card">
+    <a class="go-post" href="views/postDetail.html?id=${post.id}">
+      <div class="content">
+        <img src="${post.imageUrl}" />
+        <h3>${post.title}</h3>
+        <p>${post.body}</p>
+        <footer class="expand">
+          <small href="#" target="_blank">Expand...</small>
+        </footer>
+      </div>
+    </a>
+  </article>`;
+        document.getElementById("card-container").innerHTML += postBody;
+    });
+}
+createPost();
